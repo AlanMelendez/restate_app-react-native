120eb664995b6bcda846907575d8ca2426907823
AlanMelendez <alancuevasmelendezalan123@gmail.com>
2025-02-26 11:04:04 -0600

    Add welcome message and configure global styles with Tailwind CSS

--- app/(root)/(tabs)/index.tsx
+++ app/(root)/(tabs)/index.tsx
@@ -10,6 +10,9 @@ export default function Index() {
         alignItems: "center",
       }}
     >
+
+      <Text className="font-bold text-lg text-green-500">Welcome to ReState</Text>
+
       <Link href="/sign-in" >Sign In</Link>
       <Link href="/explore" >Explore</Link>
       <Link href="/profile" >Profile</Link>
